dd07e9e1154836fc228347929ccc6427a4ee66b3
Simon <ivonneture@gmail.com>
2023-09-25 16:16:55 +0800

    branch update

---
+++ WindowsFormsApp1/WindowsFormsApp1/Simon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        namespace WindowsFormsApp1
+{
+    internal class Elly
+    {
+        private void FuncTest()
+        {
+            Console.WriteLine("test2~");
+        }
+    }
+}
+
+	}
+}
