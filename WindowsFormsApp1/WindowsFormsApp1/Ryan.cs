d4043f85bf2fc0e3f31279fa3a53b05911706d41
Ryan <i41j7htrz@gmail.com>
2023-09-25 16:05:01 +0800

    add

---
+++ WindowsFormsApp1/WindowsFormsApp1/Ryan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Ryan
+    {
+        private void FuncTest()
+        {
+            Console.WriteLine("安安");
+        }
+    }
+}
