204522f62e6783d229478537bbb3bdadae583856
Albert <albertpcshen@gmail.com>
2023-09-25 15:39:06 +0800

    add Albert.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/Albert.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Albert
+    {
+      private void Test()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
