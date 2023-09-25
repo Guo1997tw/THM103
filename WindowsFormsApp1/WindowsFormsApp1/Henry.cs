9b4fc79fc9d1d2d56c21d247b38289550b65656e
Henry <henry00614@hotmail.com>
2023-09-25 14:40:42 +0800

    add class Henry.cs
    Signed-off-by: Henry <henry00614@hotmail.com>

---
+++ WindowsFormsApp1/WindowsFormsApp1/Henry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Henry
+    {
+        public void m1() 
+        {
+            Console.WriteLine("來使用GIT了");
+        }
+    }
+}
